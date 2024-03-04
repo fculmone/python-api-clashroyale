60735686f01f3c55119b7592d5f0037c237f40f3
Francesco Culmone <fculmone5@gmail.com>
2024-03-03 21:10:33 -0500

    properlly formatted output

--- myapp/clan_data.py
+++ myapp/clan_data.py
@@ -46,6 +46,52 @@ class ClanData:
     return data
 
 
+  def __organizeData(self, data):
+    retval = []
+    for item in data[0]['items']:
+      currentPlayer = {
+        "Name": "",
+        "Trophies": 0,
+        "Points": 0,
+        "Avg Points": 0,
+        "Weeks": 0,
+        "Donations": 0,
+        "Donations Received": 0
+      }
+
+      currentPlayer["Name"] = item["name"]
+      currentPlayer["Trophies"] = item["trophies"]
+      currentPlayer["Donations"] = item["donations"]
+      currentPlayer["Donations Received"] = item["donationsReceived"]
+
+      # Get the data for Points, Avg Points, and Weeks
+      points = 0
+      weeks = 0
+      for war in data[1]["items"]:
+        found_player = False
+        for standing in war["standings"]:
+          for participant in standing["clan"]["participants"]:
+            if participant["tag"] == item["tag"] and standing["clan"]["tag"][1:] == self.clan_tag[3:]:
+              weeks += 1
+              points += participant["fame"]
+              found_player = True
+              break
+          if found_player:
+            break
+    
+      currentPlayer["Points"] = points
+      
+      if weeks != 0:
+        currentPlayer["Avg Points"] = round(points / weeks)
+      else:
+        currentPlayer["Avg Points"] = points
+        
+      currentPlayer["Weeks"] = weeks
+
+      retval.append(currentPlayer)
+
+    return retval
+ 
   
   def getClanData(self):
     load_dotenv()
@@ -53,12 +99,12 @@ class ClanData:
     
     try:
       data = self.__callAPI(CLASH_API_KEY)
-      return data
+      return self.__organizeData(data)
     except Exception as e: 
       if "Forbidden" in str(e):
         BACKUP_CLASH_API_KEY = os.environ['BACKUP_CLASH_API_KEY']
         data = self.__callAPI(BACKUP_CLASH_API_KEY)
-        return data
+        return self.__organizeData(data)
       elif "Not Found" in str(e):
         data = [self.clan_tag]
         return data
--- myapp/routes.py
+++ myapp/routes.py
@@ -6,18 +6,13 @@ main = Blueprint('main', __name__)
 
 @main.route("/get-clan/<clan_tag>")
 def get_clan(clan_tag):
-  clan_data = {
-    "clan_tag": clan_tag,
-    "canadian_power_tag": "Q8UGY2V2",
-    "name": "Canadian Power",
-    "leader": "Mega Voltron"
-  }
-  test = ClanData(clan_tag)
-  clan_data["test"] = test.getClanData()
+  clan = ClanData(clan_tag)
+  clan_data = clan.getClanData()
   return jsonify(clan_data), 200
 
 
 @main.route("/healthy-check")
 def healthy_check(): 
+  print("hi")
   return "OK :)", 200
 
